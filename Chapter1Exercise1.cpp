aeb0574ef8e884a4318a3cf0944e1d79ef062ac3
Morgan Thorne <mt34452@email.vccs.edu>
2025-01-22 08:40:03 -0500

    Add project files.

---
+++ Chapter1Exercise1.cpp
@@ -0,0 +1,19 @@
+/*
+File Name: Chapter1Exercise1.cpp
+Programmer: Morgan Thorne
+Date: 01/2025
+Requirements:
+Create a program in C++ that accepts the number of hours worked and the pay rate of the employee 
+and display their gross pay
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
+
+
