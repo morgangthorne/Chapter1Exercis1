cb8a8cecab9edf6351269c5bfe9e99e35d8942a3
Morgan Thorne <mt34452@email.vccs.edu>
2025-01-22 08:49:38 -0500

    Modified to accept info from employee

--- Chapter1Exercise1.cpp
+++ Chapter1Exercise1.cpp
@@ -13,9 +13,13 @@ using namespace std;
 
 int main()
 {
-    int hours=40;
-    double payrate=15.00;
+    int hours;
+    double payrate;
     double pay;
+    cout << "How many hours did you work?";
+    cin >> hours;
+    cout << "How much do you earn an hour?";
+    cin >> payrate;
     pay = hours * payrate;
     cout << "Your pay check this week is $" << pay << "\n";
 }
